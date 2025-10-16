82038c842236c48d384b9a492b15644dfe3a2ed0
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2025-10-16 21:32:15 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Kode_Mono } from "next/font/google";
+import "./globals.css";
+
+const kodeMono = Kode_Mono({
+  variable: "--font-kode-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Cyber-0-Day 4.0",
+  description: "The LUGVITC Flagship CTF Event",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${kodeMono.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
