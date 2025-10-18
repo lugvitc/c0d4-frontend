404cfdac340cfcb806f1458902025ac87e0b9ac9
Sandeep Samuel <maybach9841@gmail.com>
2025-10-18 20:34:44 +0530

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex flex-1 flex-col items-center justify-center bg-black text-white">
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-black text-white">
       <h1 className="mb-8 text-5xl font-bold">Cyber-0-Day 4.0</h1>
       <p className="mb-8 text-xl">The LUGVITC Flagship CTF Event</p>
-    </main>
+    </div>
   );
 }
