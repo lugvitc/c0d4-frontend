a19c6dd888c50f50ac0014058a64c185c6e284e0
izumi <detonater9841@gmail.com>
2025-10-18 16:28:39 +0530

    Navbar implementation

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./navbar.css";
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Kode_Mono } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 
 const kodeMono = Kode_Mono({
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${kodeMono.variable} antialiased`}>{children}</body>
+      <body className={`${kodeMono.variable} antialiased`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
