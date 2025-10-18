1d852d005acf2494c46620aeff9392c14a871265
izumi <detonater9841@gmail.com>
2025-10-18 15:53:15 +0530

    Navbar implementation

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import styles from "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>Cyber-0-Day 4.0</div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="/">Sign Up</Link>
+        </li>
+        <li>
+          <Link href="/">Timeline</Link>
+        </li>
+        <li>
+          <Link href="/">Rules</Link>
+        </li>
+        <li>
+          <Link href="/">Leaderboard</Link>
+        </li>
+        <li>
+          <Link href="/">Prizes</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
