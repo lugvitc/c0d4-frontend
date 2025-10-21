5f14dfa7dfe6e6b5721946abe110d945824e9a4c
izumi <detonater9841@gmail.com>
2025-10-21 23:14:34 +0530

    Responsivity changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center bg-black text-white">
+    <div className="fixed inset-0 flex-col items-center justify-center bg-black text-white">
       <h1 className="mb-8 text-5xl font-bold">Cyber-0-Day 4.0</h1>
       <p className="mb-8 text-xl">The LUGVITC Flagship CTF Event</p>
     </div>
