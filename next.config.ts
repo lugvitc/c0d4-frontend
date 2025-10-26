9906fe24f3ded794336eccf3c7dd3467aa48014d
TheChilledBuffalo <abhishekgs.tvm@gmail.com>
2025-10-26 23:30:29 +0530

    add deployment script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/c0d4-frontend",
 };
 
 export default nextConfig;
